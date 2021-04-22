c919d9154f874fc2fb552c4e082c74c4d9e0f8c3
Eric Brown <56092687+evvic@users.noreply.github.com>
2021-04-22 15:08:32 +0300

    initialized

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+using System.Threading.Tasks;
+//using System.Drawing;
+
+namespace Face_Detection
+{
+    public partial class Form1 
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(713, 12);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 38);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "&Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click_1);
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic.Location = new System.Drawing.Point(12, 12);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(687, 402);
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//emgu library
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System.Drawing.Imaging;
+
+/*
+ * The current reason this program is not working is believed to be
+ * that the XML file containing the facial recognition data is not
+ * formatted correctly. The idea is that the file should be cloned
+ * onto a folder ton this PC then dragged into this project rather
+ * than being copied and pasted.
+ * The Tutorial I've been following is complete except for the
+ * current error: https://www.youtube.com/watch?v=zLgIy0o_0Ow
+ * The XML file is from the openCV repo which should be cloned:
+ * https://github.com/opencv/opencv/tree/master/data/haarcascades
+ * I created a clone folder for this under /source/
+ */
+
+namespace Face_Detection
+{
+    public partial class Form1 : Form
+    {
+        //retrieve face recognition data
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        public Form1() { InitializeComponent(); }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void btnDetect_Click_1(object sender, EventArgs e)
+        {
+            //allow user to open a file dialog and select image to scan
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, Filter = "JPEG|*.jpg" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pic.Image = Image.FromFile(ofd.FileName);
+                    Bitmap bitmap = new Bitmap(pic.Image);
+
+                    //creating an Image from a bitmap is no longer supported in the latest version of emgu.
+                    //instead of downgrading, add Emgu.CV.Bitmap from NuGet then use .ToImage<>() 
+
+                    //NEW METHOD .ToImage ~ Emgu.CV.Bitmap
+                    Image<Bgr, byte> greyImage = bitmap.ToImage<Bgr, byte>();
+
+                    Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(greyImage, 1.4, 0);
+
+                    //draw rectangles around detected faces
+                    foreach (Rectangle rectangle in rectangles)
+                    {
+                        using (Graphics graphics = Graphics.FromImage(bitmap))
+                        {
+                            using (Pen pen = new Pen(Color.Red, 1))
+                            {
+                                graphics.DrawRectangle(pen, rectangle);
+                            }
+                        }
+                    }
+                    pic.Image = bitmap;
+                }
+            }
+        }
+    }
+}
